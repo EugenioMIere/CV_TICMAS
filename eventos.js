a59db56540ab3e3508455869d4268b8230a3ebb4
Eugenio Miere <emierearrua@alumno.unlam.edu.ar>
2023-03-08 22:31:50 -0300

    agregando footer

--- eventos.js
+++ eventos.js
@@ -1,9 +1,34 @@
-document.getElementById('bper').onclick = function (){
+document.getElementById('btn_perfil').onclick = function (){
     console.log("capturamos el evento click");
-    document.getElementById('perfil').style='display:none';
+    document.getElementById('habilidades').style='display: none';
+    document.getElementById('experiencia').style='display: none';
+    document.getElementById('contacto').style='display: none';
+    document.getElementById('perfil').style='display: block';
 }
 
+document.getElementById('btn_habilidades').onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById('perfil').style='display: none';
+    document.getElementById('experiencia').style='display: none';
+    document.getElementById('contacto').style='display: none';
+    document.getElementById('habilidades').style='display: block';
+}
 
+document.getElementById('btne_xperiencia').onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById('habilidades').style='display: none';
+    document.getElementById('perfil').style='display: none';
+    document.getElementById('contacto').style='display: none';
+    document.getElementById('experiencia').style='display: block';
+}
+
+document.getElementById('btn_contacto').onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById('habilidades').style='display: none';
+    document.getElementById('experiencia').style='display: none';
+    document.getElementById('perfil').style='display: none';
+    document.getElementById('contacto').style='display: block';
+}
 
 
 
